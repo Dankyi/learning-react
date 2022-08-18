5d2c84432b6390b92072bb4f33b64bb65d395cc6
Dankyi <u1669769@unimail.hud.ac.uk>
2022-08-18 16:39:04 +0100

    cryptoapp - initial app layout design

---
+++ cryptoapp/src/components/index.js
@@ -0,0 +1,8 @@
+// This file allows importation of all
+// components to be used in App component
+export { default as Navbar } from "./Navbar";
+export { default as Homepage } from "./Homepage";
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
+export { default as CryptoDetails } from "./CryptoDetails";
+export { default as Exchanges } from "./Exchanges";
+export { default as News } from "./News";
