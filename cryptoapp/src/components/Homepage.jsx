4061b69b2ce625cdca6677df323eead080ccb877
Dankyi <u1669769@unimail.hud.ac.uk>
2022-08-18 21:18:18 +0100

    cryptoapp: tied some of the fetched data properties to the homepage

--- cryptoapp/src/components/Homepage.jsx
+++ cryptoapp/src/components/Homepage.jsx
@@ -9,17 +9,23 @@ const { Title } = Typography;
 const Homepage = () => {
     // Now call the api as redux hook
     const { data, isFetching } = useGetCryptosQuery();
-    console.log(data);
+    // console.log(data);
+    const globalStats = data?.data?.stats;
+
+    // From console logging the data we can see data logged 
+    // before it finally got printed so that's why Redux 
+    // provides the isFetching hook so we check before..
+    if (isFetching) return "Loading...";
 
     return (
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value="5" /></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
-                <Col span={12}><Statistic title="Total Market Cap" value="5" /></Col>
-                <Col span={12}><Statistic title="Total 24hr Volume" value="5" /></Col>
-                <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+                <Col span={12}><Statistic title="Total 24hr Volume" value={millify(globalStats.total24hVolume)} /></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
             </Row>
         </>
     );
