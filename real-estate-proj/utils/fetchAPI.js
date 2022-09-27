65f8735c81e51713dd551c028dab3c177bdc003b
Dankyi <dankyiboateng15@gmail.com>
2022-09-27 17:59:25 +0100

    real-estate-proj: removed publicly displayed keys

--- real-estate-proj/utils/fetchAPI.js
+++ real-estate-proj/utils/fetchAPI.js
@@ -7,7 +7,7 @@ export const fetchAPI = async (url) => {
     // after getting the response from the api call
     const { data } = await axios.get(url, {
         headers: {
-            'X-RapidAPI-Key': 'f80c00bcccmshf51878c7f9960a3p142d1ejsnc993cf7340cc',
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_API_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     });
