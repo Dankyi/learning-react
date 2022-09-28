19ab32d76a6f0765d3c88ed5ac0780c8fc5f4b76
Dankyi <dankyiboateng15@gmail.com>
2022-09-28 17:11:35 +0100

    google-proj: implemented darkmode functionality

--- google-proj/src/components/Navbar.jsx
+++ google-proj/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ darkTheme, setDarkTheme }) => {
     return (
-        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+        <div className="p-4 pb-2 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
             <div className="flex justify-between items-center space-x-5 w-screen">
                 <Link to="/">
-                    <p className="text-2xl">
-                        Google 🔍
+                    <p className="text-2xl font-bold text-white py-1 px-2 rounded dark:bg-gray-500 bg-blue-500 dark:text-gray-900">
+                        GoogleProj 🔍
                     </p>
                 </Link>
+
+                <button 
+                    className="text-xl dark:bg-gray-50 bg-white dark:text-gray-900 border rounded-full px-2 py-1 hover:shadow-lg"
+                    type="button"
+                    onClick={() => setDarkTheme(!darkTheme)}
+                >
+                    {darkTheme ? "Light 💡" : "Dark 🌙"}
+                </button>
             </div>
         </div>
     );
