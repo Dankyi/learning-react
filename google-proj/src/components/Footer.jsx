fb88231febd1ae8e744945e8b728617b0a264985
Dankyi <dankyiboateng15@gmail.com>
2022-09-28 19:52:21 +0100

    google-proj: footer implementation

--- google-proj/src/components/Footer.jsx
+++ google-proj/src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+            <h1>2022 GoogleProj, Inc.</h1>
         </div>
     );
 }
